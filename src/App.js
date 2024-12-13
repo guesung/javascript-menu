81349e98619525f1dd0376e253ca5ce676dd6367
guesung <gueit214@naver.com>
2024-12-13 14:48:48 +0900

    feat: 돌아가는 깡통 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-unreachable-loop */
+const { Random } = require('@woowacourse/mission-utils');
 const InputView = require('./views/InputView.js');
+const { MENUS } = require('./lib/constants.js');
+const CoachModel = require('./models/CoachModel.js');
 
 const SAMPLE = {
   일식: '규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼',
@@ -7,11 +12,47 @@ const SAMPLE = {
   아시안: '팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜',
   양식: '라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니',
 };
-
 class App {
+  #coachModels;
+
   async play() {
     const coachs = await InputView.readCoach();
-    console.log(coachs);
+    this.#coachModels = coachs.map((coach) => new CoachModel(coach));
+
+    for await (const coachModel of this.#coachModels) {
+      const foodNotEat = await InputView.readFoodNotEat(coachModel.name);
+      coachModel.setFoodNotEat(foodNotEat);
+    }
+
+    const categories = [];
+    for (let day = 0; day < 5; day += 1) {
+      while (1) {
+        const categoryIndex = Random.pickNumberInRange(1, 5);
+        const category = Object.keys(MENUS)[categoryIndex - 1];
+        if (categories.filter((it) => it === category).length < 2) {
+          categories.push(category);
+          break;
+        }
+      }
+    }
+
+    for (let day = 0; day < 5; day += 1) {
+      const category = categories[day];
+      const menus = MENUS[category];
+
+      for await (const coachModel of this.#coachModels) {
+        while (1) {
+          const menuIndex = Random.shuffle(new Array(9).fill().map((_, index) => index))[0];
+          const menu = menus[menuIndex];
+          const canEatFood = coachModel.checkEatFood(menu);
+
+          if (!canEatFood) continue;
+
+          coachModel.setFood(menu);
+          break;
+        }
+      }
+    }
   }
 }
 
